210d7542a176ffa2df39c482ba131444ddd31f96
juanrinke <rinkejuan@gmail.com>
2022-06-09 20:42:34 -0300

    ultima correcion entrega coder

---
+++ proyecto_coder/app_coder/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+
+class Curso_formulario(forms.Form):
+
+    nombre = forms.CharField(max_length=40)
+    camada = forms.IntegerField()
+
+class Alumno_formulario(forms.Form):
+    nombre = forms.CharField(max_length=40)
+    camada = forms.IntegerField()
+    nacimiento = forms.DateField()
+
+class Profesor_formulario(forms.Form):
+    nombre = forms.CharField(max_length=40)
+    legajo = forms.IntegerField()
